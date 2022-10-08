8c4ae1d41b5885615292f8d5f2fb4caa142b0e6d
ilyas <73260385+Mr-TalhaIlyas@users.noreply.github.com>
2022-10-08 10:42:06 +0900

    Update gray2rgb.py

--- scripts/utils/gray2rgb.py
+++ scripts/utils/gray2rgb.py
@@ -1,25 +1,42 @@
-import tensorflow as tf
 import numpy as np
+import cv2
 
-def gray2rgb(gray_processed, pallet):
+def gray2rgb(gray_processed, pallet, backend='np'):
     
-    pallet = tf.convert_to_tensor(pallet)
-    w, h = gray_processed.shape
-    gray = gray_processed[:,:,np.newaxis]
-    gray = tf.image.grayscale_to_rgb((tf.convert_to_tensor(gray)))
-    gray = tf.cast(gray, 'int32')
-    unq = np.unique(gray_processed)
-    rgb = tf.zeros_like(gray, dtype=tf.float64)
-    
-    for i in range(len(unq)):
-        clr = pallet[:, int(unq[i]), :]
-        clr = tf.expand_dims(clr, 0)
-        rgb = tf.where(tf.not_equal(gray,unq[i]), rgb, tf.add(rgb,clr))
+    if backend == 'tf':
         
-    if tf.executing_eagerly()==False:
-        sess = tf.compat.v1.Session()
-        rgb = sess.run(rgb)
-    else:
-        rgb = rgb.numpy()
+        import tensorflow as tf
+        
+        pallet = tf.convert_to_tensor(pallet)
+        w, h = gray_processed.shape
+        gray = gray_processed[:,:,np.newaxis]
+        gray = tf.image.grayscale_to_rgb((tf.convert_to_tensor(gray)))
+        gray = tf.cast(gray, 'int32')
+        unq = np.unique(gray_processed)
+        rgb = tf.zeros_like(gray, dtype=tf.float64)
+        
+        for i in range(len(unq)):
+            clr = pallet[:, int(unq[i]), :]
+            clr = tf.expand_dims(clr, 0)
+            rgb = tf.where(tf.not_equal(gray,unq[i]), rgb, tf.add(rgb,clr))
+            
+        if tf.executing_eagerly()==False:
+            sess = tf.compat.v1.Session()
+            rgb = sess.run(rgb)
+        else:
+            rgb = rgb.numpy()
+            
+    elif backend == 'np':
+        gray = gray_processed
+        w, h = gray.shape
+        gray = gray[:,:,np.newaxis]
+        gray = cv2.cvtColor(gray,cv2.COLOR_GRAY2RGB)
+        
+        unq = np.unique(gray)
+        rgb = np.zeros((w,h,3))
+        
+        for i in range(len(unq)):
+            clr = pallet[:,unq[i],:]
+            rgb = np.where(gray!=unq[i], rgb, np.add(rgb,clr))
+            
     return rgb
-
